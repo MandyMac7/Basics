e61175741db1c68a4c792ca0a816c8164a754930
MandyMac7 <nshmakani@gmail.com>
2024-10-17 18:01:00 +0200

    added variables

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 console.log("Hello, World!");
+let firstName = "Mandy";
+let age = 34;
+let color = "purple";
+
+console.log(typeof color);
+console.log(`Your firstName is ${firstName}`);
+console.log(`Your age is ${age}`);
+console.log(`Your color is ${color}`);
